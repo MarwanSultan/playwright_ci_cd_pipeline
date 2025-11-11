73a0b26d18e9ee7ab1d8e0ac81b365c3f6b3f5cf
Marwan <marwan.sultan@gmail.com>
2025-11-11 15:54:24 -0500

    Merge init_setup into main, unrelated histories

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -6,4 +6,5 @@ test('test', async ({ page }) => {
   await page.getByRole('searchbox', { name: 'Search Amazon' }).press('Enter');
   await page.getByRole('button', { name: 'Go', exact: true }).click();
   await expect(page.locator('.a-link-normal.s-no-outline').first()).toBeVisible();
+  
 });
